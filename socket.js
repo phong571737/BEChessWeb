bcd5b4d793635ea077894d08939ed9f3de4cdfa8
phong571737 <phong571737@gmail.com>
2026-01-30 23:12:39 +0700

    add mongdb

--- socket.js
+++ socket.js
@@ -2,6 +2,7 @@ const { createServer } = require("http");
 const { Server } = require("socket.io");
 const express = require("express");
 const bodyParser = require("body-parser");
+const {connectDB, getMoveCollections} = require("./db")
 const PORT = process.env.PORT || 8080;
 
 const app = express();
@@ -10,27 +11,42 @@ const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
     origin: ["https://chessweb-five.vercel.app"], //domain frontend
-    methods: ["GET", "POST"],
+    methods: ["POST"],
     credentials: true
   }
 });
 
 app.use(bodyParser.json());
 
-app.post("/move", (req, res) =>{
-  const data = req.body; //uci or pgn
-  console.log("Move from pico", data);
+// Connect DB
+connectDB().catch(console.error);
 
-  io.emit("pico_move", data);// send to web
-  res.json({
-    status: "ok"
-  })
+// Get move from ESP
+app.post("/move", (req, res) =>{
+  try{
+    const data = req.body; //uci or pgn
+    console.log("Move from Esp", data);
+
+    const moves = getMoveCollections();
+
+    const doc = {
+      ...data, 
+      createdAt: new Date(),
+    };
+
+    io.emit("esp_move", data);// send to web
+    res.json({
+      status: "ok"
+    })
+  }catch (err){
+    console.error(err);
+  }
 })
 
 io.on("connection", (socket) => {
   socket.emit('i am connected');
 });
 
-httpServer.listen(PORT, "0.0.0.0", function () {
-  console.log("Server is running at port " + PORT);
-});
+// httpServer.listen(PORT, "0.0.0.0", function () {
+//   console.log("Server is running at port " + PORT);
+// });
