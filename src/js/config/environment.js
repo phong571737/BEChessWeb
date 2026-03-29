dfed03bbad81b3de54c01eb845c601eb26c031cb
phong571737 <phong571737@gmail.com>
2026-03-29 22:06:22 +0700

    mqtt

--- src/js/config/environment.js
+++ src/js/config/environment.js
@@ -7,5 +7,10 @@ export const env = {
     AUTHOR: process.env.AUTHOR,
     PORT: process.env.PORT,
     IP_LAN: process.env.IP_LAN,
-    SERVER_NAME: process.env.SERVER_NAME
+    SERVER_NAME: process.env.SERVER_NAME,
+    URL_HIVEMQTT: process.env.URL_HIVEMQTT,
+    MQTT_USER: process.env.MQTT_USER,
+    MQTT_PASSWORD: process.env.MQTT_PASSWORD,
+    MQTT_PORT: process.env.MQTT_PORT,
+    MQTT_TOPIC_GET_IP: process.env.MQTT_TOPIC_GET_IP,
 }
